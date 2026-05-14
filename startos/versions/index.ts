ac523a7228e40cac2ee3c5a9b2889dba2e458aa7
Remco Ros <remcoros@live.nl>
2026-05-14 18:58:12 +0200

    chore: release 2.1.0:8

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_2_1_0_7 } from './v2.1.0.7'
+import { v_2_1_0_8 } from './v2.1.0.8'
 
 export const versionGraph = VersionGraph.of({
-  current: v_2_1_0_7,
+  current: v_2_1_0_8,
   other: [],
 })
---
+++ startos/versions/v2.1.0.8.ts
@@ -0,0 +1,22 @@
+import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
+import { rm } from 'fs/promises'
+
+export const v_2_1_0_8 = VersionInfo.of({
+  version: '2.1.0:8',
+  releaseNotes: {
+    en_US: 'Bump SDK to 1.5.1, remove deprecated docsUrls',
+    es_ES: 'Actualización del SDK a 1.5.1, eliminación del campo docsUrls obsoleto',
+    de_DE: 'SDK auf 1.5.1 aktualisiert, veraltetes docsUrls-Feld entfernt',
+    pl_PL: 'Aktualizacja SDK do 1.5.1, usunięcie przestarzałego pola docsUrls',
+    fr_FR: 'Mise à jour du SDK vers 1.5.1, suppression du champ docsUrls obsolète',
+  },
+  migrations: {
+    up: async ({ effects }) => {
+      // remove old start9 dir
+      await rm('/media/startos/volumes/main/start9', {
+        recursive: true,
+      }).catch(console.error)
+    },
+    down: IMPOSSIBLE,
+  },
+})
